7ce8ab8f60e054d51f2ecf37a8d59a98ef04f6d6
Gildo Junior <1gildojunior@gmail.com>
2024-01-07 23:08:58 -0300

    change website content

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = "Gildo Junior";
 export const SITE_DESCRIPTION = "Welcome to my website to show my setup.";
+export const PORTFOLIO_URL = "https://gildofj.github.io/portfolio/";
+export const TWITTER_URL = "https://twitter.com/tao_gildao";
+export const GITHUB_URL = "https://github.com/gildofj";
