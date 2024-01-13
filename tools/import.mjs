80e3477f928df903b2ec8009a8908e77189dc92a
Gildo Junior <1gildojunior@gmail.com>
2024-01-13 04:01:32 -0300

    fix live import for obsidian

--- tools/import.mjs
+++ tools/import.mjs
@@ -9,7 +9,7 @@ const home = "/home/gildofj/";
 const postsDir = path.join(home, "obsidian-vault/Uses/Posts");
 const imagesDir = path.join(home, "obsidian-vault/Uses/Images");
 const contentDir = path.join(__dirname, "src/content/posts");
-const assetsDir = path.join(__dirname, "src/assets");
+const assetsDir = path.join(__dirname, "public/assets");
 
 const createDirectoryIfNotExists = directory => {
   if (!fs.existsSync(directory)) {
@@ -23,6 +23,30 @@ const copyFile = (source, target) => {
   console.log(`Copied ${source} to ${target}`);
 };
 
+const normalizeImagesToAstroMd = filePath => {
+  const regexToGetValue = /!\[\[([^[\]]*)\]\]/g;
+  const regexToReplace = /!\[\[(.*?)\]\]/g;
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const images =
+    fileContent
+      .match(regexToGetValue)
+      ?.map(image => image.replace(regexToReplace, "")) || [];
+  const splitedFilePath = filePath.split("/");
+  const directoryName = splitedFilePath[splitedFilePath.lenght - 2];
+
+  console.log(images);
+  if (images.length) {
+    let modifiedContent = fileContent;
+    images.forEach(image => {
+      modifiedContent = modifiedContent.replace(
+        `![[${image}]]`,
+        `![${image.split(".")[0]}](/assets/${directoryName}/${image})`,
+      );
+    });
+    fs.writeFileSync(filePath, modifiedContent, "utf-8");
+  }
+};
+
 const copyFilesFromDirectory = (sourceDir, targetDir) => {
   createDirectoryIfNotExists(targetDir);
 
@@ -34,11 +58,35 @@ const copyFilesFromDirectory = (sourceDir, targetDir) => {
     if (fs.statSync(sourcePath).isDirectory()) {
       copyFilesFromDirectory(sourcePath, targetPath);
     } else {
+      normalizeImagesToAstroMd(sourcePath);
       copyFile(sourcePath, targetPath);
     }
   });
 };
 
-copyFilesFromDirectory(postsDir, contentDir);
+const watchAndCopyFiles = (sourceDir, targetDir) => {
+  copyFilesFromDirectory(sourceDir, targetDir);
 
-copyFilesFromDirectory(imagesDir, assetsDir);
+  fs.watch(sourceDir, { recursive: true }, (eventType, filename) => {
+    const sourcePath = path.join(sourceDir, filename);
+    const targetPath = path.join(targetDir, filename);
+
+    if (fs.existsSync(sourcePath)) {
+      if (fs.statSync(sourcePath).isDirectory()) {
+        copyFilesFromDirectory(sourcePath, targetPath);
+      } else {
+        normalizeImagesToAstroMd(sourcePath);
+        copyFile(sourcePath, targetPath);
+      }
+    } else {
+      // Handle file deletion here if needed
+      console.log(`File deleted: ${targetPath}`);
+    }
+  });
+
+  console.log(`Watching for changes in ${sourceDir}...`);
+};
+
+watchAndCopyFiles(postsDir, contentDir);
+
+watchAndCopyFiles(imagesDir, assetsDir);
