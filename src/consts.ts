159faea135faf466f8010be59c9ff49d7758444a
Gildo <1gildojunior@gmail.com>
2024-01-14 07:33:49 +0000

    Updates

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Gildo Junior - Uses";
-export const SITE_DESCRIPTION = "Welcome to my website to show my setup.";
-export const REPO_URL = "https://github.com/Gildofj/uses";
-export const PORTFOLIO_URL = "https://gildofj.github.io/portfolio/";
-export const TWITTER_URL = "https://twitter.com/tao_gildao";
-export const GITHUB_URL = "https://github.com/gildofj";
