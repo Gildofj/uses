ff8c14fe6b1a9ee2f9a848adc8b1c0cb9b875dbd
Gildo Junior <1gildojunior@gmail.com>
2024-01-14 05:07:25 -0300

    teste

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import addClasses from "rehype-add-classes";
 // https://astro.build/config
 export default defineConfig({
   site: "https://gildofj.github.io",
-  base: "/uses",
+  base: "/uses/",
   integrations: [
     sitemap(),
     react({
