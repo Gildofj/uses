d06f9a64caa67e77b2e321ec11a997bd0af56563
Gildo Junior <1gildojunior@gmail.com>
2024-01-19 12:40:35 -0300

    translate website description and title

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Gildo Junior - Uses";
-export const SITE_DESCRIPTION = "Welcome to my website to show my setup.";
+export const SITE_TITLE = "Setup | Gildo Junior";
+export const SITE_DESCRIPTION = "Bem vindo ao website do meu Setup.";
 export const REPO_URL = "https://github.com/Gildofj/uses";
 export const PORTFOLIO_URL = "https://gildofj.github.io/portfolio/";
 export const TWITTER_URL = "https://twitter.com/tao_gildao";
