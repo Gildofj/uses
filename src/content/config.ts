710f2dfa446844024b9357ae050f1f0b6ea7cca5
Gildo Junior <1gildojunior@gmail.com>
2024-01-22 11:10:16 -0300

    fix build and cover iamge

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,7 +9,7 @@ const posts = defineCollection({
     // Transform string to Date object
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    heroImage: z.string().optional().nullable(),
     tags: z.string().array().optional(),
     videos: z.string().array().optional(),
   }),
