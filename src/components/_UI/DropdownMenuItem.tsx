6696f729c1d72f270c3052befd954239032a3fd1
Gildo Junior <1gildojunior@gmail.com>
2025-12-31 14:30:06 -0300

    refactor: update icon imports and usage for consistency across components

--- src/components/_UI/DropdownMenuItem.tsx
+++ src/components/_UI/DropdownMenuItem.tsx
@@ -20,6 +20,7 @@ export default function DropdownMenuItem({
 }: Props) {
   return (
     <MenuItem
+      as="div"
       {...(href && { href })}
       {...(onClick && { onClick })}
       className={classNames(
