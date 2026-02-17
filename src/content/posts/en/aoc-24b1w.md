5aca2fd4f0b0bfa0451c7a1755b869be2eafd38f
Gildo Junior <1gildojunior@gmail.com>
2026-02-17 06:23:21 -0300

    fix: slow image rendering

--- src/content/posts/en/aoc-24b1w.md
+++ src/content/posts/en/aoc-24b1w.md
@@ -4,8 +4,9 @@ description: Monitor
 pubDate: 2024-01-14T15:12Z
 tags:
   - Computação
-heroImage: /assets/aoc-24b1w/aoc-24b1w.jpg
+heroImage: /src/content/images/aoc-24b1w/aoc-24b1w.jpg
 ---
+
 Currently, this is the monitor I use as my primary display. I find it to be excellent in terms of cost-benefit, considering its features like up to 75Hz refresh rate, 24 inches in size, and full HD resolution. It has a great size, allowing for easy separation into two windows without space issues, significantly improving my day-to-day productivity. So far, it's perfect for my daily use.
 
 Another crucial point for me is its excellent performance in gaming, with a very sharp image even though it's not a top-of-the-line monitor.
--- tools/import.mjs
+++ tools/import.mjs
@@ -29,7 +29,7 @@ const vaultPath = findObsidianVault();
 const postsDir = path.join(vaultPath, "Uses/Posts");
 const imagesDir = path.join(vaultPath, "Uses/Images");
 const contentDir = path.join(__dirname, "src/content/posts");
-const assetsDir = path.join(__dirname, "public/assets");
+const contentImagesDir = path.join(__dirname, "src/content/images");
 
 const normalizePath = path => path.replace(/ /g, "-").toLowerCase();
 
@@ -138,7 +138,7 @@ const isCI = process.env.CI === "true" || process.env.GITHUB_ACTIONS === "true";
 if (isCI) {
   console.log("Running in CI environment. Performing one-time sync.");
   syncFilesBetweenDirectories(postsDir, contentDir);
-  syncFilesBetweenDirectories(imagesDir, assetsDir);
+  syncFilesBetweenDirectories(imagesDir, contentImagesDir);
   console.log("Sync completed.");
 } else {
   const watchAndCopyFiles = (sourceDir, targetDir) => {
@@ -162,5 +162,5 @@ if (isCI) {
   };
 
   watchAndCopyFiles(postsDir, contentDir);
-  watchAndCopyFiles(imagesDir, assetsDir);
+  watchAndCopyFiles(imagesDir, contentImagesDir);
 }
